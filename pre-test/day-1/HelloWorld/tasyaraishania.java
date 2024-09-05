9741c226a9eeb043a0a59b303c0413886db42a59
Tasya <tasyaraishania@gmail.com>
2024-09-05 10:47:36 +0700

    add constants

--- pre-test/day-1/HelloWorld/tasyaraishania.java
+++ pre-test/day-1/HelloWorld/tasyaraishania.java
@@ -1,28 +1,34 @@
 public class tasyaraishania {
     public static void main (String[] args) {
 
-        System.out.println("Hello, World!");
+//        System.out.println("Hello, World!");
+//
+//        long tabungan;
+//        tabungan = 9223372036854775807L;
+//
+//        int umur;
+//        umur =  24;
+//
+//        float berat;
+//        berat = 49.5f;
+//
+//        char inisial;
+//        inisial = 'T';
+//
+//        boolean isGirl;
+//        isGirl = true;
+//
+//        System.out.println("Jumlah tabungan: " + tabungan);
+//        System.out.println("Umur: "+  umur);
+//        System.out.println("Berat badan: " + berat);
+//        System.out.println("Inisial: " + inisial);
+//        System.out.println("Perempuan? " + isGirl);
 
-        long tabungan;
-        tabungan = 9223372036854775807L;
-
-        int umur;
-        umur =  24;
-
-        float berat;
-        berat = 49.5f;
-
-        char inisial;
-        inisial = 'T';
-
-        boolean isGirl;
-        isGirl = true;
-
-        System.out.println("Jumlah tabungan: " + tabungan);
-        System.out.println("Umur: "+  umur);
-        System.out.println("Berat badan: " + berat);
-        System.out.println("Inisial: " + inisial);
-        System.out.println("Perempuan? " + isGirl);
+        final float phi = 3.14f;
+        int r1 = 10;
+        System.out.println("Luas lingkaran 1= " + phi*r1*r1);
+        r1 = 15;
+        System.out.println("Luas lingkaran 2= " + phi*r1*r1);
 
     }
 }

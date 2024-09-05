b7a75d2360621210a0c6ac992a139e83c0083075
Tasya <tasyaraishania@gmail.com>
2024-09-05 14:21:31 +0700

    my employee

---
+++ pre-test/day-1/section-2/my-employee/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    String name;
+    String npk;
+    int salary;
+    String address;
+    int age;
+
+    void sayHello() {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ pre-test/day-1/section-2/my-employee/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee();
+        employee1.name = "Tasya";
+        employee1.npk = "68769";
+        employee1.salary = 10000000;
+        employee1.address = "Bekasi";
+        employee1.age = 24;
+        employee1.sayHello();
+    }
+}

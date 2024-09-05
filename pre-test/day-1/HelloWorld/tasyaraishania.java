7a1bfbd06355e0a92208f1b539871dff2ecb8d01
Tasya <tasyaraishania@gmail.com>
2024-09-05 10:35:06 +0700

    add hello world

--- pre-test/day-1/HelloWorld/tasyaraishania.java
+++ pre-test/day-1/HelloWorld/tasyaraishania.java
@@ -1,5 +1,28 @@
 public class tasyaraishania {
     public static void main (String[] args) {
+
         System.out.println("Hello, World!");
+
+        long tabungan;
+        tabungan = 9223372036854775807L;
+
+        int umur;
+        umur =  24;
+
+        float berat;
+        berat = 49.5f;
+
+        char inisial;
+        inisial = 'T';
+
+        boolean isGirl;
+        isGirl = true;
+
+        System.out.println("Jumlah tabungan: " + tabungan);
+        System.out.println("Umur: "+  umur);
+        System.out.println("Berat badan: " + berat);
+        System.out.println("Inisial: " + inisial);
+        System.out.println("Perempuan? " + isGirl);
+
     }
 }
